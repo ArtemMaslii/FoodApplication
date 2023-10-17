06d29c13b83cb1e5cd50a08e69c9107eb04f0fd3
Artem <maslijartei@gmail.com>
2023-10-17 13:33:19 +0200

    Add repositories for User with additional methods to find by phone number; add MenuCard repository

---
+++ src/main/java/com/example/practice/repositories/UsersRepository.java
@@ -0,0 +1,16 @@
+package com.example.practice.repositories;
+
+import com.example.practice.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsersRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByName(String name);
+
+    Optional<User> findByPhone(String phone);
+}
+
