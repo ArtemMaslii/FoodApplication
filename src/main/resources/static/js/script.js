d3aa53fc403e98eb7df61c52b7e857a30d9d956e
Artem <maslijartei@gmail.com>
2023-10-17 16:43:48 +0200

    Translate text from russian variant just for now. Fix date in script

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Timer
 
-    const deadline = '2022-06-11';
+    const deadline = '2024-05-20';
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
