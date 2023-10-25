9b5360fa37fcc8c8a3f5fbd06f91577567ee1298
Artem <maslijartei@gmail.com>
2023-10-25 12:43:56 +0200

    Edit files to increase readability by passing arguments to a function. To sliders file was passed destructured object with arguments. Also fixed a bug with white images instead of photos by parsing widthInPixels

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -10,11 +10,20 @@ import {openModal} from "./modules/modals";
 window.addEventListener('DOMContentLoaded', function() {
     const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 30000);
 
-    tabs();
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modals('[data-modal]', '.modal', modalTimerId);
-    timer();
-    sliders();
-    calculator();
-    forms(modalTimerId);
+    timer('.timer', '2024-05-20');
     cards();
+    calculator('.calculating__result span');
+    forms('form', modalTimerId);
+    sliders({
+        container: ".offer__slider",
+        slide: ".offer__slide",
+        nextArrow: ".offer__slider-next",
+        prevArrow: ".offer__slider-prev",
+        totalCounter: "#total",
+        currentCounter: "#current",
+        wrapper: ".offer__slider-wrapper",
+        field: ".offer__slider-inner"
+    });
 });
